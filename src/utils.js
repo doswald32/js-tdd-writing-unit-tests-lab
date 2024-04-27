8fd94b1fe84da57dc8f140df4726915010cdd509
doswald32 <dan.oswald32@gmail.com>
2024-04-27 16:22:25 -0600

    completed lab

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,11 @@
-// Your code here
+export function isPalindrome(word) {
+    let match = [];
+    for (const char of word) {
+        match.unshift(char);
+    }
+    if (match === word) {
+        return true;
+    } else {
+        return false;
+    }
+}
